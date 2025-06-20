858e00077b37c68e8b11daf2af3c4f7d30e81479
Lucas <lucasbertoncello1@gmail.com>
2025-06-20 16:42:46 -0300

    feat: add server and routers

---
+++ src/server/index.ts
@@ -0,0 +1,37 @@
+import express from "express";
+
+import { SignUpController } from "../application/controllers/SignUpController";
+import { SignUpUseCase } from "../application/useCases/SignUpUseCase";
+
+import { SignInUseCase } from "../application/useCases/SignInUseCase";
+import { SignInController } from "../application/controllers/SignInController";
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/sign-up", async (request, response) => {
+  const signUpUseCase = new SignUpUseCase();
+  const signUpController = new SignUpController(signUpUseCase);
+
+  const { statusCode, body } = await signUpController.handle({
+    body: request.body,
+  });
+
+  response.status(statusCode).json(body);
+});
+
+app.post("/sign-in", async (request, response) => {
+  const signInUseCase = new SignInUseCase();
+  const signInController = new SignInController(signInUseCase);
+
+  const { body, statusCode } = await signInController.handle({
+    body: request.body,
+  });
+
+  response.status(statusCode).json(body);
+});
+
+app.listen(3001, () => {
+  console.log("🔥 Server started at http://localhost:3001");
+});
