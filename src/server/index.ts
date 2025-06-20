07558c9fb42ff455b3867a5a71e1ea701a0d999e
Lucas <lucasbertoncello1@gmail.com>
2025-06-20 17:05:17 -0300

    refactor: applied factory pattern

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,18 +1,14 @@
 import express from "express";
 
-import { SignUpController } from "../application/controllers/SignUpController";
-import { SignUpUseCase } from "../application/useCases/SignUpUseCase";
-
-import { SignInUseCase } from "../application/useCases/SignInUseCase";
-import { SignInController } from "../application/controllers/SignInController";
+import { makeSignUpController } from "../factories/SignUp/makeSignUpController";
+import { makeSignInController } from "../factories/SignIn/makeSignInController";
 
 const app = express();
 
 app.use(express.json());
 
 app.post("/sign-up", async (request, response) => {
-  const signUpUseCase = new SignUpUseCase();
-  const signUpController = new SignUpController(signUpUseCase);
+  const signUpController = makeSignUpController();
 
   const { statusCode, body } = await signUpController.handle({
     body: request.body,
@@ -22,8 +18,7 @@ app.post("/sign-up", async (request, response) => {
 });
 
 app.post("/sign-in", async (request, response) => {
-  const signInUseCase = new SignInUseCase();
-  const signInController = new SignInController(signInUseCase);
+  const signInController = makeSignInController();
 
   const { body, statusCode } = await signInController.handle({
     body: request.body,
