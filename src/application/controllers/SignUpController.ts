bad8e319bc05470f6a62296703eb4b090513d7ab
Lucas <lucasbertoncello1@gmail.com>
2025-06-20 15:36:55 -0300

    feat: add signup controller

---
+++ src/application/controllers/SignUpController.ts
@@ -0,0 +1,35 @@
+import { IController, IRequest, IResponse } from "../interfaces/IController";
+import { SignUpUseCase } from "../useCases/SignUpUseCase";
+import { z, ZodError } from "zod";
+
+const schema = z.object({
+  name: z.string().min(2),
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+export class SignUpController implements IController {
+  constructor(private readonly signUpUseCase: SignUpUseCase) {}
+
+  async handle(request: IRequest): Promise<IResponse> {
+    try {
+      const { name, email, password } = schema.parse(request.body);
+
+      await this.signUpUseCase.execute({ name, email, password });
+
+      return {
+        statusCode: 204,
+        body: null,
+      };
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return {
+          statusCode: 400,
+          body: error.issues,
+        };
+      }
+
+      return error;
+    }
+  }
+}
